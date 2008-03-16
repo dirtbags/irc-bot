dd677d5d4e93d645e6672322d5bd210caee31694
Neale Pickett <neale@woozle.org>
2008-03-15 20:01:53 -0600

    Dispatch tests don't need their own module

--- tests.ml
+++ tests.ml
@@ -44,7 +44,7 @@ let epoll_expect e ?(n=3) l =
 
 let unit_tests =
   "Unit tests" >::: [
-    "epoll" >:: 
+    "Epoll" >:: 
       (fun () -> 
          let a,b = Unix.socketpair Unix.PF_UNIX Unix.SOCK_STREAM 0 in
          let e = Epoll.create 1 in
@@ -93,6 +93,33 @@ let unit_tests =
 
            Unix.close b;
            Epoll.destroy e
+      );
+
+    "Dispatch" >:: 
+      (fun () ->
+         let d = Dispatch.create 3 in
+         let a,b = Unix.socketpair Unix.PF_UNIX Unix.SOCK_STREAM 0 in
+         let rec handle fd events =
+           match events with
+             | [Dispatch.Input; Dispatch.Output] ->
+                 let s = String.create 4096 in
+                 let n = Unix.read fd s 0 4096 in
+                   assert_equal
+                     n
+                     (Unix.write fd s 0 n)
+             | _ ->
+                 ()
+         in
+           assert_equal 2 (Unix.write a "hi" 0 2);
+           Dispatch.add d b handle [Dispatch.Input; Dispatch.Output];
+           Dispatch.once d;
+           let s = String.create 4096 in
+             assert_equal 2 (Unix.read a s 0 4096);
+             assert_equal "hi" (Str.string_before s 2);
+
+             Dispatch.destroy d;
+             Unix.close a;
+             Unix.close b
       );
 
       "command_of_string" >:: 
@@ -217,6 +244,6 @@ let regression_tests =
 
 let _ =
   Irc.name := "testserver.test";
-  run_test_tt_main (TestList [Dispatch_tests.unit; unit_tests; regression_tests])
+  run_test_tt_main (TestList [unit_tests; regression_tests])
 	
   
